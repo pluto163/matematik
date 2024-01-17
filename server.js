2a24c478a66de3e04c75129ed88ef5ab1cac99c5
pluto163 <yunusemreozer2008xx@gmail.com>
2024-01-18 02:46:05 +0300

    tes1

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const http = require("http")
 const url = require("url")
 const port = process.env.PORT || 3030
+const fs = require("fs")
+console.log("started")
  
 http.createServer((req,res)=>{
     console.log("connection")
     var pathName = url.parse(req.url,true).pathname
+    console.log(pathName)
+    if(pathName == "/"){
+        fs.readFile("index.html",(err,data)=>{
+            console.log("reading")
+            if(err){console.log(err)}
 
+            else{
+            res.writeHead(200, {
+                "Content-Type":"text/html"
+            })
+            res.write(data)
+            res.end()
+            }
+
+            
+        })
+    }
+    else{res.end("404 not found :'(")}
 
     
-    res.end(pathName)
 }).listen(port)
 
 console.log("listening on "+port) 
