6be71651ba7268218f1d91ddda013b20edb885d6
pluto163 <yunusemreozer2008xx@gmail.com>
2024-01-17 23:35:50 +0300

    port selection

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-http = require("http")
-url = require("url")
-    
-
+const http = require("http")
+const url = require("url")
+const express = require("express")
+ 
 http.createServer((req,res)=>{
     console.log("connection")
 
     res.end("hello world")
-}).listen("8080")
+}).listen(process.env.PORT || 3030)
 
 console.log("listening")
