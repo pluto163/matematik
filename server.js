83ce611d8898d1de1adc58ea4030ee72e67ebbd0
pluto163 <yunusemreozer2008xx@gmail.com>
2024-01-18 00:15:42 +0300

    added logging

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const http = require("http")
 const url = require("url")
+const port = process.env.PORT || 3030
  
 http.createServer((req,res)=>{
     console.log("connection")
+    var pathName = url.parse(req.url,true).pathname
 
-    res.end("hello world")
-}).listen(process.env.PORT || 3030)
 
-console.log("listening")
+    
+    res.end(pathName)
+}).listen(port)
+
+console.log("listening on "+port) 
